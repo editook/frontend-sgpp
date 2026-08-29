74ea757d9bb364ae76429f4b0bc81fd33359ad7b
editook <edito.ok123@gmail.com>
2026-08-28 23:09:02 -0400

    config base path react

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/gestion-pericial/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 })
